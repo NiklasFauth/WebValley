d10a571672ce6b1f495862b82a9342e7cbac4ba8
Niklas Fauth <niklasf1997@gmx.de>
2015-06-23 14:53:16 +0200

    added python script

--- tutorial.py
+++ tutorial.py
@@ -1,14 +1,35 @@
-'''
-Created on 23.06.2015
+from Tkinter import *
 
-@uthor: niklas
-'''
+master = Tk()
+master.wm_title("Calculator") #Makes the title that will appear in the top left
+e = Entry(master)
+e.pack()
 
+e.focus_set()
 
+f = Entry(master)
+f.pack()
 
-def mysum (d, e):
-    return d + e
+f.focus_set()
+
+def callback():
+    zahl1 = int(e.get())
+    zahl2 = int(f.get())
+    print zahl1
+    print zahl2
+    print "Result: ",
+    print zahl2 + zahl1
     
-    
-print mysum(2, 4)    
+
+b = Button(master, text="add", width=10, command=callback)
+b.pack()
+
+mainloop()
+
+e = Entry(master, width=50)
+e.pack()
+
+f = Entry(master, width=50)
+f.pack()
+
 
